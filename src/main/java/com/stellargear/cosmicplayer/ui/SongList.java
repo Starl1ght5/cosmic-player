359d93acfd2da87329137884d9145652a1767a82
DimeWolf <137341769+Starl1ght5@users.noreply.github.com>
2026-07-09 23:13:09 -0500

    refactor: change SongList from String-based to Song-based with custom SongCell

--- src/main/java/com/stellargear/cosmicplayer/ui/SongList.java
+++ src/main/java/com/stellargear/cosmicplayer/ui/SongList.java
@@ -2,30 +2,33 @@ package com.stellargear.cosmicplayer.ui;
 
 import java.util.List;
 
+import com.stellargear.cosmicplayer.services.PlayerService.Song;
+
 import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.scene.control.ListView;
 
 public class SongList {
 
-    private final ListView<String> listView = new ListView<>();
-    
+    private final ListView<Song> listView = new ListView<>();
+
         public SongList() {
+             listView.setCellFactory(lv -> new SongCell());
         }
-    
-        public ListView<String> getNode() {
+
+        public ListView<Song> getNode() {
             return listView;
         }
-    
-        public void setItems(List<String> items) {
+
+        public void setItems(List<Song> items) {
             listView.setItems(FXCollections.observableArrayList(items));
         }
-    
-        public ReadOnlyObjectProperty<String> selectedSongProperty() {
+
+        public ReadOnlyObjectProperty<Song> selectedSongProperty() {
             return listView.getSelectionModel().selectedItemProperty();
         }
-    
-        public String getSelected() {
+
+        public Song getSelected() {
             return listView.getSelectionModel().getSelectedItem();
         }
 }
