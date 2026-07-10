f86ed2e5f2fe8b427b5304949243fbbf83572c84
DimeWolf <137341769+Starl1ght5@users.noreply.github.com>
2026-07-09 23:53:52 -0500

    feat: add button to select music folder

--- src/main/java/com/stellargear/cosmicplayer/ui/SidePanel.java
+++ src/main/java/com/stellargear/cosmicplayer/ui/SidePanel.java
@@ -1,5 +1,6 @@
 package com.stellargear.cosmicplayer.ui;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
@@ -7,12 +8,17 @@ public class SidePanel {
 
     private final VBox layout = new VBox();
     private final Label home = new Label("Home");
+    private final Button chooseFolderBtn = new Button("Elegir carpeta");
 
     public SidePanel() {
-        layout.getChildren().addAll(home);
+        layout.getChildren().addAll(home, chooseFolderBtn);
     }
 
     public VBox getNode() {
         return layout;
+    }
+
+    public Button getChooseFolderBtn() {
+        return chooseFolderBtn;
     }
 }
