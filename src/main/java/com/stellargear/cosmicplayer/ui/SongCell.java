495c5c70812434015a3fe174855d7f6c54f1e370
DimeWolf <137341769+Starl1ght5@users.noreply.github.com>
2026-07-21 22:51:17 -0500

    fix: stop SongCell from constantly redrawing

--- src/main/java/com/stellargear/cosmicplayer/ui/SongCell.java
+++ src/main/java/com/stellargear/cosmicplayer/ui/SongCell.java
@@ -5,15 +5,23 @@ import com.stellargear.cosmicplayer.services.PlayerService.Song;
 import javafx.scene.control.ListCell;
 
 public class SongCell extends ListCell<Song> {
+    private SongListItem item;
+
     @Override
     protected void updateItem(Song song, boolean empty) {
         super.updateItem(song, empty);
+    
         if (empty || song == null) {
             setText(null);
             setGraphic(null);
-        } else {
-            SongListItem item = new SongListItem(song.title(), song.artist(), song.duration());
+            return;
+        }
+    
+        if (item == null) {
+            item = new SongListItem(song.file(), song.title(), song.artist(), song.duration(), song.coverArt());
             setGraphic(item.getNode());
+        } else {
+            item.update(song.file(), song.title(), song.artist(), song.duration(), song.coverArt());
         }
     }
 }
