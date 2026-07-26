4a39673736ad7860d87461a647b0eae64043566a
DimeWolf <137341769+Starl1ght5@users.noreply.github.com>
2026-07-26 15:10:23 -0500

    style: add columns to SongListItem

--- src/main/java/com/stellargear/cosmicplayer/ui/SongListItem.java
+++ src/main/java/com/stellargear/cosmicplayer/ui/SongListItem.java
@@ -4,7 +4,10 @@ import java.io.File;
 
 import com.stellargear.cosmicplayer.utils.Methods;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
@@ -12,30 +15,48 @@ import javafx.scene.image.ImageView;
 
 public class SongListItem {
 
-    private HBox songTimeBox;
-    private HBox generalBox;
-
-    private VBox controlBox;
-
     private Label songName = new Label("");
     private Label artistName = new Label("");
     private Label songDuration = new Label("");
     private Label albumName = new Label("");
 
+    private GridPane generalBox = new GridPane();
+
     private final ImageView coverArtBox = new ImageView();
 
-    public SongListItem (File songFile, String song, String artist, String album, String duration, byte[] imageData) {
-        songTimeBox = new HBox(8, songName, songDuration);
-        controlBox = new VBox(songTimeBox, artistName, albumName);
-        generalBox = new HBox(10, coverArtBox, controlBox);
+    private final HBox songNameBox = new HBox(8, songName);
+    private final  HBox imageBox = new HBox(coverArtBox);
+    private final HBox extraBox = new HBox(songDuration);
 
-        coverArtBox.setFitWidth(50);
-        coverArtBox.setFitHeight(50);
+    private final VBox controlBox = new VBox(songNameBox, artistName, albumName);
+    
+    public SongListItem (File songFile, String song, String artist, String album, String duration, byte[] imageData) {
+        
+        coverArtBox.setFitWidth(60);
+        coverArtBox.setFitHeight(60);
         coverArtBox.setPreserveRatio(true);
+
+        imageBox.setAlignment(Pos.CENTER);
+        extraBox.setAlignment(Pos.CENTER_RIGHT);
+
+        ColumnConstraints left = new ColumnConstraints();
+        left.setPercentWidth(10);
+        ColumnConstraints center = new ColumnConstraints();
+        center.setPercentWidth(80);
+        center.setMinWidth(0);
+        ColumnConstraints right = new ColumnConstraints();
+        right.setPercentWidth(10);
+        generalBox.getColumnConstraints().addAll(left, center, right);
+        generalBox.setMinWidth(0);
+
+        generalBox.add(imageBox, 0, 0);
+        generalBox.add(controlBox, 1, 0);
+        generalBox.add(extraBox, 2, 0);
 
         generalBox.getStyleClass().add("list-item");
         albumName.getStyleClass().add("album-name-label");
         artistName.getStyleClass().add("artist-name-label");
+        controlBox.getStyleClass().add("control-box");
 
         Rectangle clip = new Rectangle(coverArtBox.getFitWidth(), coverArtBox.getFitHeight());
         clip.setArcWidth(30);
@@ -50,10 +71,10 @@ public class SongListItem {
         artistName.setText(artist);
         songDuration.setText(duration);
         albumName.setText(album);
-        coverArtBox.setImage(Methods.toCachedThumbnail(songFile, imageData, 50));
+        coverArtBox.setImage(Methods.toCachedThumbnail(songFile, imageData, 60));
     }
 
-    public HBox getNode () {
+    public GridPane getNode () {
         return generalBox;
     }
 }
